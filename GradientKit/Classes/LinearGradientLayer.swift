d18be99b160fd2e8727cd6961d05839438047129
Jamie Chapman <jamiechapman@users.noreply.github.com>
2018-09-08 16:29:24 +0200

    Update LinearGradientLayer.swift

--- GradientKit/Classes/LinearGradientLayer.swift
+++ GradientKit/Classes/LinearGradientLayer.swift
@@ -9,7 +9,7 @@
 import Foundation
 import QuartzCore
 
-public class LinearGradientLayer: GradientLayer {
+@objcMembers public class LinearGradientLayer: GradientLayer {
     
     public enum Direction {
         case horizontal
